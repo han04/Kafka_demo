57eb18891617017f44f07f29a8fe42883e4de69c
han04 <hans01091@outlook.com>
2022-08-21 00:44:38 +0800

    Producer 普通异步发送 不带回调函数

---
+++ src/main/java/com/kafka/producer/CustomProducer.java
@@ -0,0 +1,32 @@
+package com.kafka.producer;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class CustomProducer {
+
+    public static void main(String[] args) {
+        String SERIALIZER = "org.apache.kafka.common.serialization.StringSerializer";
+
+        //创建Kafka生产者的配置对象
+        Properties properties = new Properties();
+        //给Kafka配置对象添加信息 BOOTSTRAP_SERVERS
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"hadoop102:9092");
+        //key,value 序列化
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, SERIALIZER);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, SERIALIZER);
+        //创建Kafka生产者对象
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<String,String>(properties);
+        //调用send方法，发送消息
+        for (int i = 0; i < 5; i++) {
+            kafkaProducer.send(new ProducerRecord<>("first","message" + i));
+        }
+
+        //关闭producer
+        kafkaProducer.close();
+
+    }
+}
