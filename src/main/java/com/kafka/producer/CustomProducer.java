562532588fced00f4b56132a670bb6feb79132d4
han04 <hans01091@outlook.com>
2022-08-21 13:44:39 +0800

    Producer 普通异步发送 不带回调函数

--- src/main/java/com/kafka/producer/CustomProducer.java
+++ src/main/java/com/kafka/producer/CustomProducer.java
@@ -3,21 +3,21 @@ package com.kafka.producer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
 
 public class CustomProducer {
 
     public static void main(String[] args) {
-        String SERIALIZER = "org.apache.kafka.common.serialization.StringSerializer";
 
         //创建Kafka生产者的配置对象
         Properties properties = new Properties();
         //给Kafka配置对象添加信息 BOOTSTRAP_SERVERS
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"hadoop102:9092");
         //key,value 序列化
-        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, SERIALIZER);
-        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, SERIALIZER);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         //创建Kafka生产者对象
         KafkaProducer<String, String> kafkaProducer = new KafkaProducer<String,String>(properties);
         //调用send方法，发送消息
